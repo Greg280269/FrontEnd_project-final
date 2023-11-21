5b7ec09ff0e0ac46e78b0904f5210eaea70b3636
Greg280269 <113647750+Greg280269@users.noreply.github.com>
2023-11-20 21:59:50 -0500

    deploy

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(request: JwtRequest) {
-    return this.http.post("http://localhost:8080/authenticate", request);
+    return this.http.post("https://evt-service.onrender.com/authenticate", request);
   }
   verificar() {
     let token = sessionStorage.getItem("token");
